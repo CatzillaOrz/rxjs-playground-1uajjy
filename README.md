12af61f24d7d47e4f30edbca7544146e70bf2de3
CatzillaOrz <catzillaorz@outlook.com>
2022-06-16 17:33:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-1uajjy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-1uajjy)
---
+++ index.ts
@@ -0,0 +1,7 @@
+import './style.css';
+
+// import { of, map, Observable } from 'rxjs';
+
+// of('World')
+//   .pipe(map((name) => `Hello, ${name}!`))
+//   .subscribe(console.log);

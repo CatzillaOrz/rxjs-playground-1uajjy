6f67583b370d0ada4155bd0a2d340a4ed2b006b2
CatzillaOrz <catzillaorz@outlook.com>
2022-07-05 15:16:25 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -3,4 +3,6 @@ import { runMyFun } from './0-runFun';
 import { TestRxjsUnsubscribe } from './cancellation-unsubscribe';
 import { CreateObserable } from './01-obserable';
 
+// runMyFun(TestRxjsUnsubscribe);
+
 runMyFun(CreateObserable);

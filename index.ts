96536f4aab83a0d684ae32041e3dc2223cfed66f
CatzillaOrz <catzillaorz@outlook.com>
2022-07-05 17:40:54 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import './style.css';
 import { runMyFun } from './0-runFun';
 import { TestRxjsUnsubscribe } from './cancellation-unsubscribe';
-import { CreateObserable } from './01-observable-create';
-import { ObservableMulti } from './02-observable-multi';
+import { CreateObserable } from './01-observable_create';
+import { ObservableMulti } from './02-observable_multi';
 
 // runMyFun(TestRxjsUnsubscribe);
 

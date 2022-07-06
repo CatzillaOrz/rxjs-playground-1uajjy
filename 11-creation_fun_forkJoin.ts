344ae0efa95645c6bc1b30fa0c532b36cdb1c835
CatzillaOrz <catzillaorz@outlook.com>
2022-07-06 17:12:24 +0800

    1 file modified

--- 11-creation_fun_forkJoin.ts
+++ 11-creation_fun_forkJoin.ts
@@ -1,5 +1,12 @@
+import { forkJoin } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import { CommonRandomUri } from './0-ConstConfig';
+
+/*
+ ** {forkJoin}
+ ** forkJoin([A,B])
+ **
+ */
 
 export class CreationFunctionForkJoin {
   create() {
@@ -7,19 +14,27 @@ export class CreationFunctionForkJoin {
     const randomCity$ = ajax(CommonRandomUri.RANDOM_CITY);
     const randomFood$ = ajax(CommonRandomUri.RANDOM_FOOD);
 
-    randomName$.subscribe({
-      next: (value) => {
-        console.log('value:', value);
-      },
-    });
-    randomCity$.subscribe({
-      next: (value) => {
-        console.log('value:', value);
-      },
-    });
-    randomFood$.subscribe({
-      next: (value) => {
-        console.log('value:', value);
+    // randomName$.subscribe({
+    //   next: (value) => {
+    //     console.log('value:', value);
+    //   },
+    // });
+    // randomCity$.subscribe({
+    //   next: (value) => {
+    //     console.log('value:', value);
+    //   },
+    // });
+    // randomFood$.subscribe({
+    //   next: (value) => {
+    //     console.log('value:', value);
+    //   },
+    // });
+
+    forkJoin([randomName$, randomCity$, randomFood$]).subscribe({
+      next: ([nameAjax, cityAjax, foodAjax]: any[]) => {
+        console.log(
+          `${nameAjax.response.name} \n is from \n ${cityAjax.response.capital} \n and likes eat \n ${foodAjax.response.dish}`
+        );
       },
     });
   }

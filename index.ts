9b1d4db6185b647b70f84ef6b02668aecbe74115
CatzillaOrz <catzillaorz@outlook.com>
2022-07-06 10:40:54 +0800

    1 file modified, 1 added, 1 deleted

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import './style.css';
 import { runMyFun } from './0-runFun';
 import { CreateObserable } from './01-observable_create';
 import { ObservableMulti } from './02-observable_multi';
-import { RxjsUnsubscribe } from './04-cancellation-unsubscribe';
+import { RxjsUnsubscribe } from './04-cancellation_unsubscribe';
 
 runMyFun(RxjsUnsubscribe);
 

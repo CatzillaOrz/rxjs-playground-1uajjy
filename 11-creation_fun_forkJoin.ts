3d5de4c7c93e1d6f15ae293569354a0b3eff4445
CatzillaOrz <catzillaorz@outlook.com>
2022-07-06 17:45:57 +0800

    1 file modified

--- 11-creation_fun_forkJoin.ts
+++ 11-creation_fun_forkJoin.ts
@@ -1,6 +1,7 @@
-import { forkJoin } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import { CommonRandomUri } from './0-ConstConfig';
+import { unsubscibe } from './0-unSub';
 
 /*
  ** {forkJoin}
@@ -39,7 +40,46 @@ export class CreationFunctionForkJoin {
     });
   }
 
+  createError() {
+    const a$ = new Observable((subscriber) => {
+      setTimeout(() => {
+        subscriber.next('A');
+        subscriber.complete();
+      }, 3000);
+      return () => {
+        console.log('a$ is Teardown!!');
+
+        subscriber.unsubscribe();
+      };
+    });
+    const b$ = new Observable((subscriber) => {
+      setTimeout(() => {
+        subscriber.error('Failure!');
+        // subscriber.next('B');
+        // subscriber.complete();
+      }, 5000);
+      return () => {
+        console.log('b$ is Teardonw!!');
+        subscriber.unsubscribe();
+      };
+    });
+    const subscription$ = forkJoin([a$, b$]).subscribe({
+      next: (value) => {
+        console.log('value:', value);
+      },
+      error: (error) => {
+        console.log('error:', error);
+        unsubscibe([subscription$]);
+      },
+      complete: () => {
+        console.log('unsubscribe forkJoin subscription:');
+        unsubscibe([subscription$]);
+      },
+    });
+  }
+
   run() {
-    this.create();
+    this.createError();
+    // this.create();
   }
 }

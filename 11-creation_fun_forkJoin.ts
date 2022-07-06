2a5c4dbaa88736e082eb45c6209f259d271359de
CatzillaOrz <catzillaorz@outlook.com>
2022-07-06 16:58:22 +0800

    1 file modified

--- 11-creation_fun_forkJoin.ts
+++ 11-creation_fun_forkJoin.ts
@@ -1,6 +1,27 @@
+import { ajax } from 'rxjs/ajax';
+import { CommonRandomUri } from './0-ConstConfig';
+
 export class CreationFunctionForkJoin {
   create() {
-    throw new Error('Method not implemented.');
+    const randomName$ = ajax(CommonRandomUri.RANDOM_NAME);
+    const randomCity$ = ajax(CommonRandomUri.RANDOM_CITY);
+    const randomFood$ = ajax(CommonRandomUri.RANDOM_FOOD);
+
+    randomName$.subscribe({
+      next: (value) => {
+        console.log('value:', value);
+      },
+    });
+    randomCity$.subscribe({
+      next: (value) => {
+        console.log('value:', value);
+      },
+    });
+    randomFood$.subscribe({
+      next: (value) => {
+        console.log('value:', value);
+      },
+    });
   }
 
   run() {

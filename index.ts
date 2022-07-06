c2dbcd670a21185af5519d2569c8f44b53b9b2fa
CatzillaOrz <catzillaorz@outlook.com>
2022-07-06 10:41:33 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { CreateObserable } from './01-observable_create';
 import { ObservableMulti } from './02-observable_multi';
 import { RxjsUnsubscribe } from './04-cancellation_unsubscribe';
 
-runMyFun(RxjsUnsubscribe);
+// runMyFun(RxjsUnsubscribe);
 
 // runMyFun(CreateObserable);
 

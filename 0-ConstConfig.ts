17e8f0bcfb1c899e3e3100d76d3a929e1ee45a95
CatzillaOrz <catzillaorz@outlook.com>
2022-07-06 16:53:48 +0800

    1 file modified

--- 0-ConstConfig.ts
+++ 0-ConstConfig.ts
@@ -1,3 +1,5 @@
-export enum CommonRandomUri{
-  RANDOM_NAME = "https://random-data-api.com/api/name/random_name"
+export enum CommonRandomUri {
+  RANDOM_NAME = 'https://random-data-api.com/api/name/random_name',
+  RANDOM_CITY = 'https://random-data-api.com/api/nation/random_nation',
+  RANDOM_FOOD = 'https://random-data-api.com/api/food/random_food',
 }

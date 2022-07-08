610d53a71fb3b0ed532a48236b2b4c61bbe2fc87
CatzillaOrz <catzillaorz@outlook.com>
2022-07-08 18:10:34 +0800

    2 files modified

--- 18-flattening-operator_concatMap.ts
+++ 18-flattening-operator_concatMap.ts
@@ -1,7 +1,22 @@
-import { concatMap, Observable, of } from 'rxjs';
+import { concatMap, fromEvent, map, Observable, of, tap } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 export class FlatteningOperatorConcatMap {
   create() {
+    const endpointInput = document.getElementById('endpoint');
+    const fetchButton = document.getElementById('concat_pipe_fetch');
+
+    fromEvent(fetchButton, 'click')
+      .pipe(
+        tap((value) => console.log(endpointInput.value)),
+        map(() => endpointInput['value']),
+        concatMap((value) =>
+          ajax(`http://random-data-api.com/api/${value}/random_${value}`)
+        )
+      )
+      .subscribe((value) => console.log(value));
+  }
+  createBase() {
     const source$ = new Observable((subscriber) => {
       setTimeout(() => {
         subscriber.next('A');
@@ -14,14 +29,12 @@ export class FlatteningOperatorConcatMap {
       };
     });
     const subscription$ = source$
-    .pipe(
-      concatMap(value => of(1,2))
-    )
-    .subscribe({
-      next: (value) => {
-        console.log('value:', value);
-      },
-    });
+      .pipe(concatMap((value) => of(1, 2)))
+      .subscribe({
+        next: (value) => {
+          console.log('value:', value);
+        },
+      });
   }
 
   run() {

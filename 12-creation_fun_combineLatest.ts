b4ada641d2a765b0ed2acd9645d21e208247f0a7
CatzillaOrz <catzillaorz@outlook.com>
2022-07-08 15:28:22 +0800

    2 files modified

--- 12-creation_fun_combineLatest.ts
+++ 12-creation_fun_combineLatest.ts
@@ -5,21 +5,32 @@
  **
  */
 
-import { combineLatest } from 'rxjs';
+import { combineLatest, fromEvent } from 'rxjs';
 
 export class CreationFunctionCombineLatest {
   create() {
     const conversionDropdown = document.getElementById('conversion-dropdown');
     const temperatureInput = document.getElementById('temperature-input');
+    const resultBox = document.getElementById('result-text');
 
-    const temperatureInputEvent$ = formEvent(temperatureInput, 'input');
+    const temperatureInputEvent$ = fromEvent(temperatureInput, 'input');
 
-    const conversionInputEvent$ = formEvent(conversionDropdown, 'input');
+    const conversionInputEvent$ = fromEvent(conversionDropdown, 'input');
 
     combineLatest([temperatureInputEvent$, conversionInputEvent$]).subscribe(
-      ([temperatureInputEvent, conversionDropdownEvent]) => {
-        console.log(temperatureInputEvent.target.value);
-        console.log(conversionDropdownEvent.target.value);
+      ([event0, event1]: any[]) => {
+        const temperature = event0.target.value;
+        const conversion = event1.target.value;
+        let result: number;
+        switch (conversion) {
+          case 'f-to-c':
+            result = ((temperature - 32) * 5) / 9;
+            break;
+          case 'c-to-f':
+            result = (temperature * 9) / 5 + 32;
+            break;
+        }
+        resultBox.innerHTML = String(result);
       }
     );
   }
@@ -27,8 +38,4 @@ export class CreationFunctionCombineLatest {
   run() {
     this.create();
   }
-}
-
-function formEvent(temperatureInput: HTMLElement, arg1: string) {
-  throw new Error('Function not implemented.');
 }

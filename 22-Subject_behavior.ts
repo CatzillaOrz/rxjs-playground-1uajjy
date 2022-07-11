084d9a6801b1ddcdaa5d959bae27c26e509807e7
CatzillaOrz <catzillaorz@outlook.com>
2022-07-11 15:43:24 +0800

    2 files modified

--- 22-Subject_behavior.ts
+++ 22-Subject_behavior.ts
@@ -7,12 +7,14 @@
  */
 
 import { BehaviorSubject, fromEvent, Subject } from 'rxjs';
+import { withLatestFrom } from 'rxjs/operators';
 
 export class SubjectBehavior {
   create() {
     const loginButton = document.getElementById('login');
     const logoutButton = document.getElementById('logout');
     const loggedInStatus = document.getElementById('login-status');
+    const listeningButton = document.getElementById('listening');
 
     const isLoggedIn$ = new BehaviorSubject<boolean>(false);
 
@@ -33,7 +35,15 @@ export class SubjectBehavior {
       loginButton.style.display = !value ? 'block' : 'none';
       logoutButton.style.display = value ? 'block' : 'none';
     });
+
+    fromEvent(listeningButton, 'click')
+      .pipe(withLatestFrom(isLoggedIn$))
+      .subscribe(([event, value]) => {
+        console.log('listening:', value);
+        console.log('event:', event);
+      });
   }
+
   run() {
     this.create();
   }
